8947c302fb729c40a3efe77152619b403610ee4e
Brandon Wilson <brandon@coil.com>
2020-10-22 22:52:08 -0500

    remove balances endpoints

--- src/services/Redis.ts
+++ src/services/Redis.ts
@@ -8,17 +8,12 @@ import { Receipt } from 'ilp-protocol-stream'
 
 interface CustomRedis extends ioredis.Redis {
   getReceiptValue(key: string, tempKey: string, streamId: string, amount: string): Promise<string>
-  creditBalance(key: string, amount: string): Promise<string>
-  spendBalance(key: string, amount: string): Promise<string>
 }
 
 interface CustomRedisMock extends ioredisMock {
   getReceiptValue(key: string, tempKey: string, streamId: string, amount: string): Promise<string>
-  creditBalance(key: string, amount: string): Promise<string>
-  spendBalance(key: string, amount: string): Promise<string>
 }
 
-export const BALANCE_KEY = 'ilpBalances'
 export const RECEIPT_KEY = 'ilpReceipts'
 export const SPSP_ENDPOINT_KEY = 'spspEndpoint'
 const TEMP_KEY = 'ilpTemp'
@@ -63,34 +58,6 @@ else
 end
 `
     })
-
-    this.redis.defineCommand('creditBalance', {
-      numberOfKeys: 1,
-      lua: `
-local amount = ARGV[1]
-redis.call('incrby', KEYS[1], amount)
-return redis.call('get', KEYS[1])
-`
-    })
-
-    this.redis.defineCommand('spendBalance', {
-      numberOfKeys: 1,
-      lua: `
-if redis.call('get', KEYS[1]) then
-  local amount = ARGV[1]
-  redis.call('decrby', KEYS[1], amount)
-  local balance = redis.call('get', KEYS[1])
-  if string.sub(balance, 1, 1) == '-' then
-    redis.call('incrby', KEYS[1], amount)
-    return redis.error_reply('insufficient balance')
-  else
-    return balance
-  end
-else
-  return redis.error_reply('balance does not exist')
-end
-`
-    })
   }
 
   async stop (): Promise<void> {
@@ -128,37 +95,5 @@ end
     } else {
       return Long.UZERO
     }
-  }
-
-  async getBalance (id: string): Promise<Long | null> {
-    const key = `${BALANCE_KEY}:${id}`
-    const balance = await this.redis.get(key)
-    return balance ? Long.fromString(balance) : null
-  }
-
-  async creditBalance (id: string, amount: Long): Promise<Long> {
-    if (amount.isNegative()) {
-      throw new Error('credit amount must not be negative')
-    } else if (amount.compare(Long.MAX_VALUE) === 1) {
-      throw new Error('credit amount exceeds max 64 bit signed integer')
-    }
-    const key = `${BALANCE_KEY}:${id}`
-    try {
-      const balance = await this.redis.creditBalance(key, amount.toString())
-      return Long.fromString(balance)
-    } catch (err) {
-      throw new Error('balance cannot exceed max 64 bit signed integer')
-    }
-  }
-
-  async spendBalance (id: string, amount: Long): Promise<Long> {
-    if (amount.isNegative()) {
-      throw new Error('spend amount must not be negative')
-    } else if (amount.compare(Long.MAX_VALUE) === 1) {
-      throw new Error('spend amount exceeds max 64 bit signed integer')
-    }
-    const key = `${BALANCE_KEY}:${id}`
-    const balance = await this.redis.spendBalance(key, amount.toString())
-    return Long.fromString(balance)
   }
 }
