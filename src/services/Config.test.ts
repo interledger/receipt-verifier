ea650c2ddc5620350e208851a99860b0a6c23b03
Brandon Wilson <brandon@coil.com>
2020-10-27 22:59:42 -0500

    encode SPSP endpoint in proxy url path

--- src/services/Config.test.ts
+++ src/services/Config.test.ts
@@ -3,30 +3,9 @@ import { Config } from './Config'
 
 describe('Config', () => {
   describe('constructor', () => {
-    it('requires SPSP_ENDPOINT or SPSP_ENDPOINTS_URL', () => {
-      try {
-        reduct()(Config)
-        fail()
-      } catch (err) {
-        expect(err.message).toBe('receipt-verifier requires SPSP_ENDPOINT or SPSP_ENDPOINTS_URL to be set')
-      }
-    })
-
     it('accepts optional RECEIPT_SEED', () => {
       process.env.RECEIPT_SEED = 'NeuPwFvsXwpp+1HzBLDzmfHZAW5Qrf3DR2NWEPwZmJg='
-      process.env.SPSP_ENDPOINT = 'http://localhost:3000'
       reduct()(Config)
-    })
-
-    it('disallows both SPSP_ENDPOINT and SPSP_ENDPOINTS_URL', () => {
-      try {
-        process.env.SPSP_ENDPOINT = 'http://localhost:3000'
-        process.env.SPSP_ENDPOINTS_URL = 'https://spsp-endpoints.net'
-        reduct()(Config)
-        fail()
-      } catch (err) {
-        expect(err.message).toBe('SPSP_ENDPOINT and SPSP_ENDPOINTS_URL are mutually exclusive')
-      }
     })
   })
 })
